2da5b32728e4e34b0690328bad63a64a6b8a8abd
kiichi1094 <k.nagayama@tbc-inc.co.jp>
2026-07-14 11:20:36 +0900

    新規登録機能実装

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return redirect('/login');
 });
 
+// ログイン
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
 Route::get('/home', function() {
     return view('home');
 })->middleware('auth');
+
+// 新規登録
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
 
 Route::post('/logout', [LoginController::class, 'logout']);
