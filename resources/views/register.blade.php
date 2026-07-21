be3a350f8bdd8aade9fcac16119a68a7325d8816
kiichi1094 <k.nagayama@tbc-inc.co.jp>
2026-07-21 16:20:03 +0900

    ログイン・新規登録画面CSS変更

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,74 +2,55 @@
 
 @section('title', '新規登録')
 
+@push('css')
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
+@endpush
+
 @section('content')
 
-@if($errors->any())
-    <ul style="color: red;">
-        @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
+<div class="auth-wrap">
 
-<form action="{{ route('register') }}" method="post">
-    @csrf
+    <div class="auth-title">新規登録</div>
 
-    <div>
-        <label for="name">名前</label><br>
-        <input 
-            type="text" 
-            id="name"
-            name="name" 
-            value="{{ old('name') }}"
-        >
+    @if($errors->any())
+        <ul class="error-list">
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <form action="{{ route('register') }}" method="post">
+        @csrf
+
+        <div class="auth-group">
+            <label for="name">名前</label>
+            <input type="text" id="name" name="name" value="{{ old('name') }}">
+        </div>
+
+        <div class="auth-group">
+            <label for="email">メールアドレス</label>
+            <input type="email" id="email" name="email" value="{{ old('email') }}">
+        </div>
+
+        <div class="auth-group">
+            <label for="password">パスワード</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+
+        <div class="auth-group">
+            <label for="password_confirmation">パスワード確認</label>
+            <input type="password" id="password_confirmation" name="password_confirmation" required>
+        </div>
+
+        <button type="submit" class="auth-button">登録する</button>
+    </form>
+
+    <div class="auth-footer">
+        すでにアカウントをお持ちの方は<br>
+        <a href="{{ route('login') }}">ログインはこちら</a>
     </div>
 
-    <br>
-
-    <div>
-        <label for="email">メールアドレス</label><br>
-        <input 
-            type="email" 
-            id="email"
-            name="email" 
-            value="{{ old('email') }}"
-        >
-    </div>
-
-    <br>
-
-    <div>
-        <label for="password">パスワード</label><br>
-        <input 
-            type="password" 
-            id="password"
-            name="password"
-            required
-        >
-    </div>
-
-    <br>
-
-    <div>
-        <label for="password_confirmation">パスワード確認</label><br>
-        <input 
-            type="password" 
-            id="password_confirmation"
-            name="password_confirmation"
-            required
-        >
-    </div>
-
-    <br>
-
-    <button type="submit">
-        登録
-    </button>
-</form>
-
-<p>
-    <a href="{{ route('login') }}">ログインはこちら</a>
-</p>
+</div>
 
 @endsection
